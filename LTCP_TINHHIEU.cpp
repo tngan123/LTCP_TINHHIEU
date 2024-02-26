69115fce8f2055788e5387a7a1a11083c352da5e
ADMIN <ADMIN@LAB1-MAY33>
2024-02-26 08:30:55 +0700

    Add project files.

---
+++ LTCP_TINHHIEU.cpp
@@ -0,0 +1,15 @@
+
+#include <iostream>
+using namespace std;
+int main() {
+	int so1, so2, hieu;
+	cout << "CHUONG TRINH TINH HIEU\N";
+	cout << "Nhap so thu nhat:";
+	cin >> so1;
+	cout << "Nhap so thu hai";
+	cin >> so2;
+	hieu = so1 - so2;
+	cout << so1 << "-" << so2 << "=" << hieu << endl;
+	return 0;
+}
+
